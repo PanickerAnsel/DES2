2e093b91faf4c8d21585829d2c6047039ba8cd7a
Ansel P <ansel.panicker@slu.edu>
2024-10-10 15:39:20 -0500

    initial

---
+++ Project2_Part2/DES.cpp
@@ -0,0 +1,14 @@
+#include "DES.h"
+#include "permutation.h"
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+string encryption(string plaintext, vector<string> keys) {
+    
+}
+
+string decryption(string encrypted, vector<string> keys) {
+    
+}
